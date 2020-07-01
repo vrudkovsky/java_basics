5d9a4f412fec1b283f5ebbcd15c26ee8fbb548b8
Victor Rudkovsky <vrudkovsky@vitasoftua.com>
2020-07-01 17:09:05 +0300

    Hello world

---
+++ h00/src/com/company/helloworld/DataTypes.java
@@ -0,0 +1,66 @@
+package ua.com.danit.fs;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // byte, short, int, long
+        // char
+        // boolean
+        // float, double
+        // null
+        // BigInteger, BigDecimal
+
+        // byte - (-128 - +127)
+
+        byte b = 126;
+        System.out.println(b++);
+        System.out.println(b++);
+        System.out.println(b++);
+        System.out.println(b++);
+
+        short s = b;
+        s += 1000;
+        int i = s;
+        long l = b;
+
+        System.out.println("short " + s);
+
+        int intCastToShort = 40000;
+        short castedShort = (short) intCastToShort;
+
+        System.out.println("int " + intCastToShort);
+        System.out.println("short" + castedShort);
+
+        System.out.println("                " + Integer.toBinaryString(intCastToShort));
+        System.out.println(Integer.toBinaryString(castedShort));
+
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(Integer.toBinaryString(5));
+
+        int empty = 0;
+
+        System.out.println(empty);
+
+        float f = 1;
+        double d = 2;
+
+        System.out.println(f);
+        System.out.println(d);
+
+        var x = 10;
+//        x = "String";
+
+        String str = null;
+
+        f = 112.9f;
+
+        int shortenedFloat = (int) f;
+
+        System.out.println(shortenedFloat);
+
+        int division = 10 / 3;
+        int remainder = 10 % 3;
+
+        System.out.println(division);
+        System.out.println(remainder);
+    }
+}
