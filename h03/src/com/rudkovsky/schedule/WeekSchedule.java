0e0672db89cf227aee5462808078283f42383615
Victor Rudkovsky <vpcomtek@gmail.com>
2020-07-10 00:30:24 +0300

    Weekly schedule WIP

--- h03/src/com/rudkovsky/schedule/WeekSchedule.java
+++ h03/src/com/rudkovsky/schedule/WeekSchedule.java
@@ -1,23 +1,34 @@
 package com.rudkovsky.schedule;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class WeekSchedule {
     public static void main(String[] args) {
         String[][] schedule = new String[7][2];
+        Scanner scanner = new Scanner(System.in);
 
         schedule[0][0] = "Sunday";
         schedule[0][1] = "to do homework; do some shopping;";
         schedule[1][0] = "Monday";
         schedule[1][1] = "go to the office; go to courses; to watch a film;";
         schedule[2][0] = "Tuesday";
-        schedule[1][1] = "go to courses; lunch with partners; to wash a car;";
+        schedule[2][1] = "go to courses; lunch with partners; to wash a car;";
         schedule[3][0] = "Wednesday";
-        schedule[1][1] = "go to courses; watch a film;";
+        schedule[3][1] = "go to courses; watch a film;";
         schedule[4][0] = "Thursday ";
-        schedule[1][1] = "go to the office; have a lunch with friends;";
+        schedule[4][1] = "go to the office; have a lunch with friends;";
         schedule[5][0] = "Friday";
-        schedule[1][1] = "go to english classes; to go to the movies; ";
+        schedule[5][1] = "go to english classes; to go to the movies; ";
         schedule[6][0] = "Saturday";
-        schedule[1][1] = "got to he gym; clean home;";
+        schedule[6][1] = "got to he gym; clean home;";
+
+        System.out.println("Please, input the day of the week: ");
+        String answer = scanner.next();
+
+        System.out.println(answer);
+
+//        System.out.println(Arrays.deepToString(schedule));
 
     }
 }
