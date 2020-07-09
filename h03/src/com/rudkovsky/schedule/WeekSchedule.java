1da5bd3a736eaa2534a9ad43bc4b6ed342029444
Victor Rudkovsky <vpcomtek@gmail.com>
2020-07-09 23:10:32 +0300

    Weekly schedule WIP

---
+++ h03/src/com/rudkovsky/schedule/WeekSchedule.java
@@ -0,0 +1,23 @@
+package com.rudkovsky.schedule;
+
+public class WeekSchedule {
+    public static void main(String[] args) {
+        String[][] schedule = new String[7][2];
+
+        schedule[0][0] = "Sunday";
+        schedule[0][1] = "to do homework; do some shopping;";
+        schedule[1][0] = "Monday";
+        schedule[1][1] = "go to the office; go to courses; to watch a film;";
+        schedule[2][0] = "Tuesday";
+        schedule[1][1] = "go to courses; lunch with partners; to wash a car;";
+        schedule[3][0] = "Wednesday";
+        schedule[1][1] = "go to courses; watch a film;";
+        schedule[4][0] = "Thursday ";
+        schedule[1][1] = "go to the office; have a lunch with friends;";
+        schedule[5][0] = "Friday";
+        schedule[1][1] = "go to english classes; to go to the movies; ";
+        schedule[6][0] = "Saturday";
+        schedule[1][1] = "got to he gym; clean home;";
+
+    }
+}
