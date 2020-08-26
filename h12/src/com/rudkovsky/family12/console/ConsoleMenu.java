dcef48b7658d341f537af0e916b5585bc27a4447
Victor Rudkovsky <vpcomtek@gmail.com>
2020-08-26 13:38:58 +0300

    Console app, Exceptions

---
+++ h12/src/com/rudkovsky/family12/console/ConsoleMenu.java
@@ -0,0 +1,23 @@
+package com.rudkovsky.family12.console;
+
+public class ConsoleMenu {
+    public static void init() {
+
+    }
+
+    public static void safeCall() {
+
+    }
+
+    public static void printMenu() {
+
+    }
+
+    public static void printSubMenu() {
+
+    }
+
+
+
+
+}
