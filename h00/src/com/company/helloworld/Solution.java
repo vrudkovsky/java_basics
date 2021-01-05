8c51e2720d7db9d07062ff5ac489d320c4600543
Victor Rudkovsky <vpcomtek@gmail.com>
2021-01-05 08:20:26 +0200

    -- smallest and largest

--- h00/src/com/company/helloworld/Solution.java
+++ h00/src/com/company/helloworld/Solution.java
@@ -12,18 +12,22 @@ public class Solution {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-        String[] str = new String[10];
+        String[] str = new String[s.length() - k + 1];
 
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
 
-//        welcometojava
-        for (int i = 0; i < s.length() - k; i++) {
+//      welcometojava
+        System.out.println(s.length());
+        for (int i = 0; i <= s.length() - k; ++i) {
             str[i] = s.substring(i, k + i);
         }
 
+        System.out.println(Arrays.toString(str));
         Arrays.sort(str);
+        System.out.println(Arrays.toString(str));
+
         smallest = str[0];
         largest = str[str.length - 1];
 
