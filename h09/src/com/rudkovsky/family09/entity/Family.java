4fd0d8ae52858a314127a4d31a78c37a4d987406
Victor Rudkovsky <vpcomtek@gmail.com>
2021-01-26 01:29:35 +0200

    -- h09

--- h09/src/com/rudkovsky/family09/entity/Family.java
+++ h09/src/com/rudkovsky/family09/entity/Family.java
@@ -1,6 +1,9 @@
 package com.rudkovsky.family09.entity;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class Family {
     private Human mother;
--- h09/src/com/rudkovsky/family09/service/FamilyService.java
+++ h09/src/com/rudkovsky/family09/service/FamilyService.java
@@ -3,10 +3,14 @@ package com.rudkovsky.family09.service;
 import com.rudkovsky.family09.dao.IFamilyDao;
 import com.rudkovsky.family09.entity.Family;
 import com.rudkovsky.family09.entity.Human;
+import com.rudkovsky.family09.entity.Man;
+import com.rudkovsky.family09.entity.Woman;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class FamilyService {
     private IFamilyDao familyDao;
@@ -63,15 +67,37 @@ public class FamilyService {
         return this.familyDao.deleteFamily(index);
     }
 
-    public Family bornChild(Family family, String motherName, String fatherName) {
-        family.addChild(new Human("son", "Alex", family.getFather().getSurname(), LocalDateTime.now().getYear(), family));
-         familyDao.saveFamily(family);
-        return family;
+    public boolean bornChild(Family family, String fatherName, String motherName){
+        if (family.getMother() == null || family.getFather() == null) {
+            return false;
+        }
+        Random random = new Random();
+        Human child;
+        int year =  new Date().getYear();
+        int iq = (family.getFather().getIq() + family.getMother().getIq()) / 2;
+        if (random.nextInt(2) == 1) {
+            String name = fatherName;
+            name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+            child = new Man("son", family.getFather().getName(), family.getFather().getSurname(),year, iq, family, null);
+        } else {
+            String name = motherName;
+            name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+            child = new Woman("daughter", family.getMother().getName(), family.getFather().getSurname(),year,iq, family,null);
+        }
+        family.addChild(child);
+        return true;
     }
 
-    public Family adoptChild(Family family, Human child) {
-
-        return new Family();
+    public boolean adoptChild(Family family, Human human) {
+        boolean check = false;
+        for (Family currFamily : this.familyDao.getAllFamilies()) {
+            if (family.equals(currFamily)) {
+                currFamily.addChild(human);
+                check = true;
+                break;
+            }
+        }
+        return check;
     }
 
 }
