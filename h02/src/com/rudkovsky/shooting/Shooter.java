073902c61c991146808df3055293fcc17d510885
Victor Rudkovsky <vpcomtek@gmail.com>
2021-03-29 03:42:22 +0300

    -- h01 code refactoring

--- h02/src/com/rudkovsky/shooting/Shooter.java
+++ h02/src/com/rudkovsky/shooting/Shooter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class AreaShooting {
+public class Shooter {
     public static void main(String[] args) {
         boolean exit = true;
 
