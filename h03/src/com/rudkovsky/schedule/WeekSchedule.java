13e0efe5765e66f8cfa3d523a2e603ba23415701
Victor Rudkovsky <vpcomtek@gmail.com>
2021-04-05 07:48:01 +0300

    -- schedule refactor

--- h03/src/com/rudkovsky/schedule/WeekSchedule.java
+++ h03/src/com/rudkovsky/schedule/WeekSchedule.java
@@ -1,14 +1,12 @@
 package com.rudkovsky.schedule;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class WeekSchedule {
-    public static void main(String[] args) {
-        String[][] schedule = new String[7][2];
-        Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
 
+    private static final String[][] schedule = new String[7][2];
+
+    public static String[][] mySchedule(String[][] schedule) {
         schedule[0][0] = "Sunday";
         schedule[0][1] = "do homework; do some shopping;";
         schedule[1][0] = "Monday";
@@ -24,42 +22,51 @@ public class WeekSchedule {
         schedule[6][0] = "Saturday";
         schedule[6][1] = "got to he gym; clean home;";
 
+        return schedule;
+    }
 
-        while (!exit) {
-            System.out.println(Arrays.deepToString(schedule));
-            System.out.println("Please, input the day of the week or exit: ");
-            String answer = scanner.next();
+    public static void printTask(String day, String task) {
+        System.out.printf("Your task for [%s] : %s\n", day, task);
+    }
 
-            if (answer.equalsIgnoreCase("exit")) {
-                exit = true;
-            } else {
-                switch (answer.toLowerCase()) {
-                    case "sunday":
-                        System.out.println(schedule[0][1] + '\n');
-                        break;
-                    case "monday":
-                        System.out.println(schedule[1][1] + '\n');
-                        break;
-                    case "tuesday":
-                        System.out.println(schedule[2][1] + '\n');
-                        break;
-                    case "wednesday":
-                        System.out.println(schedule[3][1] + '\n');
-                        break;
-                    case "thursday":
-                        System.out.println(schedule[4][1] + '\n');
-                        break;
-                    case "friday":
-                        System.out.println(schedule[5][1] + '\n');
-                        break;
-                    case "saturday":
-                        System.out.println(schedule[6][1] + '\n');
-                        break;
-                    default:
-                        System.out.println("Sorry, I don't understand you, please try again.");
-                        break;
-                }
-            }
+    public static void showTask(String day, String[][] schedule) {
+        switch (day) {
+            case "sunday":
+                printTask(day, schedule[0][1]);
+                break;
+            case "monday":
+                printTask(day, schedule[1][1]);
+                break;
+            case "tuesday":
+                printTask(day, schedule[2][1]);
+                break;
+            case "wednesday":
+                printTask(day, schedule[3][1]);
+                break;
+            case "thursday":
+                printTask(day, schedule[4][1]);
+                break;
+            case "friday":
+                printTask(day, schedule[5][1]);
+                break;
+            case "saturday":
+                printTask(day, schedule[6][1]);
+                break;
+            default:
+                System.out.println("Sorry, I don't understand you, please try again.");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[][] sch = mySchedule(schedule);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please, input the day of the week:");
+            String day = sc.nextLine().toLowerCase();
+            if (day.equals("exit")) break;
+            showTask(day, sch);
         }
     }
 }
