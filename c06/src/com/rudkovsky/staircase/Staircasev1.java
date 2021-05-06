328d15a810087184ac6f7b8110d1500e2ba28593
Victor Rudkovsky <vpcomtek@gmail.com>
2021-05-06 08:40:11 +0300

    -- staircase

--- c06/src/com/rudkovsky/staircase/Staircasev1.java
+++ c06/src/com/rudkovsky/staircase/Staircasev1.java
@@ -21,14 +21,14 @@ public class Staircasev1 {
         }
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int n = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         staircase(n);
 
-        scanner.close();
+        sc.close();
     }
 }
