ddf523ace79a33965d752a4e47065212ff43f04f
Victor Rudkovsky <vpcomtek@gmail.com>
2021-06-30 02:36:09 +0300

    -- hw06 tests

---
+++ h06/test/com/rudkovsky/family06/FamilyTest.java
@@ -0,0 +1,64 @@
+package com.rudkovsky.family06;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FamilyTest {
+
+    @Test
+    void getMother() {
+    }
+
+    @Test
+    void setMother() {
+    }
+
+    @Test
+    void getFather() {
+    }
+
+    @Test
+    void setFather() {
+    }
+
+    @Test
+    void getChildren() {
+    }
+
+    @Test
+    void setChildren() {
+    }
+
+    @Test
+    void getPet() {
+    }
+
+    @Test
+    void setPet() {
+    }
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void deleteChild() {
+    }
+
+    @Test
+    void testDeleteChild() {
+    }
+
+    @Test
+    void countFamily() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
